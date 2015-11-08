1a2beeeba1d8aad04e968f7619c4dd313e468690
Lewis <lewis.jessett@salesagility.com>
2015-11-08 14:07:09 +0000

    Hardcoded language strings for now.

--- front/js/app.js
+++ front/js/app.js
@@ -23,5 +23,29 @@ SuiteCRM.controller('SuiteView', ['$scope', '$http', '$routeParams',
         $http.get('cache/modules/'+$routeParams.mod+'/'+$routeParams.page+'.json').success(function(data) {
             $scope.panels = data.panels;
             $scope.record = {};
+            $scope.language = {
+                'LBL_BILLING_ADDRESS_STREET':'Billing Address Street',
+                'LBL_SHIPPING_ADDRESS_STREET':'Shipping Address Street',
+                'LBL_NAME':'Name',
+                'LBL_PHONE_OFFICE':'Phone Office',
+                'LBL_WEBSITE':'Website',
+                'LBL_EMAIL':'Email',
+                'LBL_DESCRIPTION':'Description',
+                'LBL_TYPE':'Type',
+                'LBL_INDUSTRY':'Industry',
+                'LBL_ANNUAL_REVENUE':'Annual Revenue',
+                'LBL_FAX':'Fax',
+                'LBL_EMPLOYEES':'Employees',
+                'LBL_CAMPAIGN':'Campaign',
+                'LBL_ASSIGNED_TO':'Assigned User',
+                'LBL_MEMBER_OF':'Member Of',
+                'LBL_ACCOUNT_INFORMATION':'Account Information',
+                'LBL_PANEL_ADVANCED':'Advanced'
+            }
+
+
         });
+        //$http.get('cache/modules/'+$routeParams.mod+'/'+$routeParams.page+'.json').success(function(data) {
+
+        //});
     }]);

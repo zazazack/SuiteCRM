832f806051d6afb2e1ac18effbbff325fe11334e
Lewis <lewis.jessett@salesagility.com>
2015-11-08 00:07:24 +0000

    Added dynamic field html creation.

--- front/js/app.js
+++ front/js/app.js
@@ -3,7 +3,6 @@ var SuiteCRM = angular.module('SuiteCRM', ['ngRoute']);
 SuiteCRM.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
-            when('/', {templateUrl: 'front/view/listview.html'}).
             when("/:mod/:page", {
                 templateUrl: function(params) {
                     return 'front/view/' + params.page + '.html';
@@ -23,5 +22,6 @@ SuiteCRM.controller('SuiteView', ['$scope', '$http', '$routeParams',
     function($scope, $http, $routeParams) {
         $http.get('cache/modules/'+$routeParams.mod+'/'+$routeParams.page+'.json').success(function(data) {
             $scope.panels = data.panels;
+            $scope.record = {};
         });
     }]);
